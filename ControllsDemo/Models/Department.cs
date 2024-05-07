4e4dbaa851784af00ffb279685b4eafd3a558eca
Kenan Kurda <intecbrussel24@gmail.com>
2024-05-07 15:54:31 +0200

    added correction

--- ControllsDemo/Models/Department.cs
+++ ControllsDemo/Models/Department.cs
@@ -3,7 +3,7 @@
     public class Department
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name{ get; set; }
         public bool IsActive { get; set; }
         public string?  SelectedOption { get; set; }
     }
